4e2f583c9feb04fa5a88ee65121b3cc64966c01c
Shubhanshi <vermashashi3421@gmail.com>
2025-01-08 11:01:18 +0530

    Question 3

---
+++ CelsiusToFahrenheitConversion.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class CelsiusToFahrenheitConversion{
+	publi static void main(String args[]){
+		Scanner sc= new Scanner(System.in);
+		double Celsius = sc.nextDouble();
+	System.out.println((Celsius * 9/5) + 32);
+	}
+}
+		
